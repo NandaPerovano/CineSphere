c00a992f6351cf5cfa79166642746ea862cef30d
NandaPerovano <nandaperovanorm@gmail.com>
2025-02-06 20:56:59 -0300

    feat: adiciona botão de favoritar na tela de home

--- CineSphere/View/HomeView.swift
+++ CineSphere/View/HomeView.swift
@@ -11,19 +11,20 @@ struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
 
     var body: some View {
-         NavigationView {
-             List(viewModel.movies) { movie in
-                 NavigationLink(destination: MovieDetailView(movie: movie)) {
-                     MovieRow(movie: movie)
-                 }
-             }
-             .navigationTitle("🎬 Filmes")
-         }
-     }
+        NavigationView {
+            List(viewModel.movies) { movie in
+                NavigationLink(destination: MovieDetailView(movie: movie)) {
+                    MovieRow(movie: movie)
+                }
+            }
+            .navigationTitle("🎬 Filmes")
+        }
+    }
 }
 
 struct MovieRow: View {
     let movie: Movie
+    @State private var isFavorite = false // Estado para controlar favorito
 
     var body: some View {
         HStack {
@@ -32,12 +33,26 @@ struct MovieRow: View {
                 .scaledToFill()
                 .frame(width: 80, height: 120)
                 .cornerRadius(8)
-
+            
             VStack(alignment: .leading, spacing: 5) {
-                Text(movie.title)
-                    .font(.headline)
-                    .foregroundColor(.primary)
+                HStack {
+                    Text(movie.title)
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    
+                    Spacer() 
+                    
+                    Button(action: {
+                        isFavorite.toggle()
+                    }) {
+                        Image(systemName: isFavorite ? "heart.fill" : "heart")
+                            .foregroundColor(isFavorite ? .red : .gray)
+                            .font(.title2)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
             }
+
             .padding(.leading, 8)
         }
         .padding(.vertical, 5)
